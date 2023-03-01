b0f03c1845ada786e5f369c362a8801924c55105
matiob <buraschi.mateo@gmail.com>
2023-03-01 10:00:14 -0300

    port config

--- api/server.js
+++ api/server.js
@@ -8,7 +8,9 @@ require("./config"); // --> require the db()
 const router = require("./routes");
 require("dotenv").config();
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
+
+console.log("PUERTO->", process.env.PORT);
 
 app.use(cors());
 app.use(compression());
